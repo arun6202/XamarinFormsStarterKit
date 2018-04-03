5020da6cf76f4f1e35c57cc441ffa7ec4f964df7
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-04-03 15:29:01 +0530

    kictstarter layout

---
+++ XamarinFormsStarterKit/XamarinFormsStarterKit/LayoutGallery/kickstarter_android-oss/XAML/DashBoardView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsStarterKit.LayoutGallery.kickstarter_androidoss.XAML
+{
+    public partial class DashBoardView : ContentPage
+    {
+        public DashBoardView()
+        {
+            InitializeComponent();
+            VisualElementColorizer.Colorizer.Colorize((Layout)Content);
+        }
+    }
+}
