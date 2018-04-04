1d9f80335f89af1c7e48dadb18d6f9470d87ca63
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-04-04 18:58:23 +0530

    push

--- XamarinFormsStarterKit/XamarinFormsStarterKit/LayoutGallery/VisualElementColorizer/XAML/Tester.xaml.cs
+++ XamarinFormsStarterKit/XamarinFormsStarterKit/LayoutGallery/VisualElementColorizer/XAML/Tester.xaml.cs
@@ -10,24 +10,28 @@ namespace XamarinFormsStarterKit.LayoutGallery.VisualElementColorizer.XAML
         public Tester()
         {
             InitializeComponent();
-            Colorizer.LoremText((Layout)Content,false);
-            Colorizer.Colorize((Layout)Content, false);
+            Colorizer.LoremText((Layout)Content,true);
+            Colorizer.Colorize((Layout)Content, true);
+            Colorizer.Image((Layout)Content, true);
 
-            int rows = 128;
-            int cols = 64;
-            BmpMaker bmpMaker = new BmpMaker(cols, rows);
 
-            //bmpMaker.SetPixel(50,50,Color.BlueViolet);
+           // image.Source = ImageSource.FromResource("XamarinFormsStarterKit.LayoutGallery.SampleImages.basicbitmaps-large.png");
 
-            for (int row = 0; row < rows; row++)
-                for (int col = 0; col < cols; col++)
-                {
-                    bmpMaker.SetPixel(row, col, 2 * row, 0, 2 * (128 - row));
+            //int rows = 128;
+            //int cols = 64;
+            //BmpMaker bmpMaker = new BmpMaker(cols, rows);
+
+            ////bmpMaker.SetPixel(50,50,Color.BlueViolet);
+
+            //for (int row = 0; row < rows; row++)
+            //    for (int col = 0; col < cols; col++)
+            //    {
+            //        bmpMaker.SetPixel(row, col, 2 * row, 0, 2 * (128 - row));
                
-                }
+            //    }
 
-            ImageSource imageSource = bmpMaker.Generate();
-            image.Source = imageSource;
+            //ImageSource imageSource = bmpMaker.Generate();
+            //image.Source = imageSource;
 
         }
     }
