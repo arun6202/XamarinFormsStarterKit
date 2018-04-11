b2cc8e45c250d0dec7ecfe8f01ba33665e63677d
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-04-11 08:37:55 +0530

    cv

---
+++ XamarinFormsStarterKit/XamarinFormsStarterKit/LayoutGallery/k0shk0sh_FastHub/XAML/contentview/Row.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamarinFormsStarterKit.LayoutGallery.k0shk0sh_FastHub.XAML.contentview
+{
+    public partial class Row : ContentView
+    {
+        public Row()
+        {
+            InitializeComponent();
+        }
+    }
+}
